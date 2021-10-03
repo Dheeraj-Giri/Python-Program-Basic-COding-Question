c6fdb356a95de20b204cf9438165e8876d679020
Dheeraj <dg598268@gmail.com>
2021-10-03 21:45:14 +0530

    update

--- timeConversion.py
+++ timeConversion.py
@@ -1,8 +1,6 @@
 '''
- Normal to Standard time conversion 
-
+Normal to Standard time conversion 
 '''
-
 def timeConversion(s):
     if ( (s[-2:]=='AM' or s[-2:]=='am')  and (s[:2]=='12') ):
         return '00'+s[2:-2]
@@ -11,5 +9,5 @@ def timeConversion(s):
     elif (( s[-2]=='PM' or s[-2:]=='PM') and (s[:2]=='12') ):
         return s[:-2]
     return  str(int(s[:2])+12)+s[2:8]
-print("Time input format is : 00:00:00 ")
+print("Time input format is : 00:00:00 AM/PM")
 print(timeConversion(input("Enter the time : ")))
